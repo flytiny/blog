885d65d100a3cca5b91b32c7333df59ec5a21d1f
diponTaken <josondi@gmail.com>
2015-05-19 17:28:53 +0800

    1.8
    右边导航1.0

--- js/index.js
+++ js/index.js
@@ -15,3 +15,39 @@ $(".main-content").click(function(){
 	};
 });
 
+	(function(){
+		var a = $('.content').find('h1,h2').length;
+		for(var i = 0;i < a;i++){
+			var tag = "tag" + i;
+			$(".content h1,.content h2").eq(i).addClass(tag);
+			$(".content h1,.content h2").eq(i).attr('id',tag);
+			var temp = '<li ><a class="a '+i+'" href="#tag'+i+'">概览</a></li>';
+			$(".right-menu-ul").append(temp);
+		}
+
+		console.log(a);
+	})();
+
+// 		var waypoint = new Waypoint(
+// 			  {
+//   element: document.getElementById("tag1"),
+//   handler: function() {
+//     $(".right-menu-ul li a").eq(1).toggleClass("active");
+//   }
+// },
+// 		{
+//   element: document.getElementById("tag4"),
+//   handler: function() {
+//     $(".right-menu-ul li a").eq(4).toggleClass("active");
+//   }
+// }
+// );
+// 				var waypoint = new Waypoint({
+//   element: document.getElementById("tag4"),
+//   handler: function() {
+//     $(".right-menu-ul li a").eq(4).toggleClass("active");
+//   }
+// });
+
+
+
