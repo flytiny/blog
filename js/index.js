10db8046112f560037d19cf70d56a63b59ac512c
diponTaken <josondi@gmail.com>
2015-05-21 11:32:55 +0800

    2.0
    侧边框完全版1.0

--- js/index.js
+++ js/index.js
@@ -14,66 +14,29 @@ $(".main-content").click(function(){
 			$("#toggle").trigger("click");
 	};
 });
-
 	(function(){
 		var a = $('.content').find('h1,h2').length;
 		for(var i = 0;i < a;i++){
 			var tag = "tag" + i;
 			$(".content h1,.content h2").eq(i).addClass(tag);
 			$(".content h1,.content h2").eq(i).attr('id',tag);
-			var temp = '<li ><a class="a '+i+'" href="#tag'+i+'">'+$(".content h1,.content h2").eq(i).html()+'</a></li>';
+			var temp = '<li ><a class="a'+i+'" href="#tag'+i+'">'+$(".content h1,.content h2").eq(i).html()+'</a></li>';
 			$(".right-menu-ul").append(temp);
 		}
-
 		console.log(a);
 	})();
-
-// 		var waypoint = new Waypoint(
-// 			  {
-//   element: document.getElementById("tag1"),
-//   handler: function() {
-//     $(".right-menu-ul li a").eq(1).toggleClass("active");
-//   }
-// },
-// 		{
-//   element: document.getElementById("tag4"),
-//   handler: function() {
-//     $(".right-menu-ul li a").eq(4).toggleClass("active");
-//   }
-// }
-// );
-// 				var waypoint = new Waypoint({
-//   element: document.getElementById("tag4"),
-//   handler: function() {
-//     $(".right-menu-ul li a").eq(4).toggleClass("active");
-//   }
-// });
-// 
-var test = $('.content').find('h1,h2');
-for (var i = 0; i < test.length; i++) {
-	(function (e) {
-  new Waypoint({
-    element: test[i],
-    handler: function(i) {
-    	// test.filter(".active").removeClass("active");
-    	// test[i].addClass("active");
-    	
-    	console.log(this.element.innerHTML());
-    },
-    continuous: false
+  $('.content').find('h1,h2').each(function() {
+    new Waypoint({
+      element: this,
+      handler: function(direction) {
+        var num = '.a'+$(this.element).attr("class").charAt(3);
+        $('.right-menu-ul').find('a').filter(".active").each(function(){
+        	$(this).removeClass("active");
+        })
+        $(num).addClass("active");
+      },
+      continuous: false
+    })  
   })
-
-	})(i)
-//   new Waypoint({
-//     element: test[i],
-//     handler: function(i) {
-//     	// test.filter(".active").removeClass("active");
-//     	// test[i].addClass("active");
-    	
-//     	console.log(this.element.css("color","red"));
-//     }
-//   })
-};
-
 
 
