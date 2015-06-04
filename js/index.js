5b09679ddf3ae2b334f1e3823f206f5cbd89b916
diponTaken <josondi@gmail.com>
2015-06-04 18:21:33 +0800

    2.3
    搜索和滚轴beta

--- js/index.js
+++ js/index.js
@@ -16,29 +16,25 @@ $("#toggle").click(
 		};
 	});
 (function() {
-		var getEle = $('.content').find('h1,h2');
-		var a = getEle.length;
-		for (var i = 0; i < a;) {
+	var getEle = $('.content').find('h1,h2');
+	var a = getEle.length;
+	for (var i = 0; i < a;) {
+		var tag = "tag" + i;
+		getEle.eq(i).addClass(tag);
+		getEle.eq(i).attr('id', tag);
+		var temp = '<li ><a class="a' + i + '" href="#tag' + i + '">' + getEle.eq(i).html() + '</a>   <ul class="nav">';
+		i = i + 1;
+		while ((i < a) && (getEle.eq(i).is('h2'))) {
 			var tag = "tag" + i;
 			getEle.eq(i).addClass(tag);
 			getEle.eq(i).attr('id', tag);
-			var temp = '<li ><a class="a' + i + '" href="#tag' + i + '">' + getEle.eq(i).html() + '</a>   <ul class="nav">';
-			i = i + 1;
-			while ((i < a) && (getEle.eq(i).is('h2'))){
-				var tag = "tag" + i;
-					getEle.eq(i).addClass(tag);
-					getEle.eq(i).attr('id', tag);
-					temp += '<li> <a class = "a'+i+' sub" href = "#tag'+i+'" > '+getEle.eq(i).html()+' </a> </li>';
-					i++;
-				}
-				temp += '</ul></li>'; $(".right-menu-ul").append(temp);
-			}
-
-
-		})(); 
-
-
-		$('.content').find('h1,h2').each(function() {
+			temp += '<li> <a class = "a' + i + ' sub" href = "#tag' + i + '" > ' + getEle.eq(i).html() + ' </a> </li>';
+			i++;
+		}
+		temp += '</ul></li>';
+		$(".right-menu-ul").append(temp);
+	}
+	$('.content').find('h1,h2').each(function() {
 		new Waypoint({
 			element: this,
 			handler: function(direction) {
@@ -46,12 +42,31 @@ $("#toggle").click(
 				$('.right-menu-ul a').removeClass('active');
 				$('.right-menu-ul ul').removeClass('active');
 				$('.right-menu-ul li ul li').removeClass('active');
-				if($(num).hasClass("sub")){
+				if ($(num).hasClass("sub")) {
 					$(num).parent().parent().addClass("active");
 				}
 				$(num).addClass("active");
 				$(num).siblings("ul").addClass("active");
+
 			},
 			continuous: false
 		})
 	})
+
+})();
+
+var totalList = $(".menu-li");
+var input = $('.search');
+input.bind('keyup', sendKeyWord);
+function sendKeyWord() {
+	dealData($.trim(input.val()));
+}
+function dealData(text) {
+	for (var i = 0; i < totalList.length; i++) {
+		if (totalList.eq(i).text().toLowerCase().indexOf(text.toLowerCase()) < 0) {
+			totalList.eq(i).hide();
+		} else {
+			totalList.eq(i).show();
+		}
+	}
+}
